4c2fa41415ae1bd1d0394115e683a3ad38d178fc
Marek <marek.horvath@student.tuke.sk>
2022-04-23 14:13:58 +0200

    uidshfufs

---
+++ visma/src/filter.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function filter() {
+  return (
+    <div className="App">
+      <header>
+        Filter
+      </header>
+    </div>
+  );
+}
+
+export default filter;
---
+++ visma/src/nav.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function nav() {
+  return (
+    <div className="App">
+      <header>
+        Navigation bar
+      </header>
+    </div>
+  );
+}
+
+export default nav;
