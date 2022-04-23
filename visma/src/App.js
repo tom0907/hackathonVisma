adb0df7b1d372d2da118a49a8c23f2ed328e6ac7
Marek <marek.horvath@student.tuke.sk>
2022-04-23 16:45:20 +0200

    telka

--- visma/src/App.js
+++ visma/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import MainBoard from "./MainBoard"
 import NewEvent from "./NewEvent"
 import Event from "./event"
+import Tv from "./tv"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
               <Route path="/" element={<MainBoard />}/>
               <Route path="/newEvent" element={<NewEvent />}/>
               <Route path="/event" element={<Event />}/>
+              <Route path="/tv" element={<Tv />}/>
             </Routes>
         </Router>
       </div>
---
+++ visma/src/tv.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './nav';
+
+function tv() {
+  return (
+    <div>
+        <Nav />
+        TU BUDU VECI PRE TELKU
+    </div>
+  );
+}
+
+export default tv;
