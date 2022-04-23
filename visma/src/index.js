6db1051478507d5d134d97d6dff641b92098e825
Marek <marek.horvath@student.tuke.sk>
2022-04-23 14:08:47 +0200

    setup

--- visma/src/index.js
+++ visma/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Nav from './nav';
+import Timeline from './timeline';
+import Filter from './filter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Nav />
+    <Timeline />
+    <Filter />
   </React.StrictMode>
 );
 
