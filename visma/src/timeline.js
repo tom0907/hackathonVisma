1f63045ad048ddfccd3391208f5394a8ca55f827
@lukas.marcincak <lukas.marcincak@student.tuke.sk>
2022-04-23 19:24:08 +0200

    daco

--- visma/src/timeline.js
+++ visma/src/timeline.js
@@ -5,11 +5,15 @@ import Komponenticek from './komponentik';
 function timeline() {
   //const child = {width: "300em",height: "20%"}
   return (
+    <div>
+<p style={{ color: 'inherit', textDecoration: 'inherit', font:'Ubuntu', fontSize:40,fontWeight: "bold",textAlign: "center"}}>Timeline</p> 
+
     <div className="timeline">
       
       <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> <Komponenticek /> 
 
       
+    </div>
     </div>
   );
 }
