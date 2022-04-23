1873f56df7d9b1313349cf4109d3f781d7738110
Marek <marek.horvath@student.tuke.sk>
2022-04-23 16:22:27 +0200

    added new files

--- visma/src/App.js
+++ visma/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import MainBoard from "./MainBoard"
 import NewEvent from "./NewEvent"
+import Event from "./event"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
               <Route path="/" element={<MainBoard />}/>
               <Route path="/newEvent" element={<NewEvent />}/>
+              <Route path="/event" element={<Event />}/>
             </Routes>
         </Router>
       </div>
--- visma/src/NewEvent.js
+++ visma/src/NewEvent.js
@@ -1,10 +1,37 @@
 import React from "react"
+import Nav from './nav';
+import {Form,Button, Card} from 'react-bootstrap'
 
 
 function NewEvent() {
   return (
       <div>
-          tu budze formularS
+          <Nav />
+          <Card>
+            <Card.Body>
+                <h2 className='text-center mb-4'> NEW EVENT</h2>
+                <Form>
+                    <Form.Group id="name">
+                        <Form.Label>Name of event</Form.Label>
+                        <Form.Control type="text" required/>
+                    </Form.Group>
+                    <Form.Group id="desctiption">
+                        <Form.Label>Desctiption</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group id="date">
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group id="photo">
+                        <Form.Label>photo</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Button className="w-100 mt-4" type="submit">Log in</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+
       </div>
   )
 }
