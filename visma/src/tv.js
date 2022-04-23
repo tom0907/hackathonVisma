0da3f0968d6aaf600d5d3abe057770d1151c50d8
Marek <marek.horvath@student.tuke.sk>
2022-04-23 19:00:03 +0200

    hodziny

--- visma/src/tv.js
+++ visma/src/tv.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Nav from './nav';
+import React from 'react';
+import Clock from 'react-live-clock';
+import Timeline from './timeline';
 
-function tv() {
-  return (
-    <div>
+function Tv() {
+
+    let newDate = new Date();
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return (
+      <div>
         <Nav />
-        TU BUDU VECI PRE TELKU
-    </div>
-  );
+        <Timeline />
+        <Clock format={'HH:mm:ss'} ticking={true} timezone={'EU/Bratislava'} className="hodiny"/>
+        <p className="hodiny">{year} {month<10?`0${month}`:`${month}`} {date}</p>
+      </div>
+    );
 }
 
-export default tv;
+export default Tv;
